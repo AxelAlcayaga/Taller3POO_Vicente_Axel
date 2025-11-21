4c2709d350500c195ed073848ff41021608fae04
vicenterojas05-beep <vicente.rojas05@alumnos.ucn.cl>
2025-11-20 23:14:29 -0300

    Delete src directory

--- src/taller3/Factory_usuarios.java
+++
@@ -1,9 +0,0 @@
-package taller3;
-
-public class Factory_usuarios {
-	public Usuario crear_usuario(String username, String password, String rol) {
-		return new Usuario(username,password,rol);
-		
-	}
-
-}
--- src/taller3/IPriorizar.java
+++
@@ -1,9 +0,0 @@
-package taller3;
-
-import java.util.ArrayList;
-
-public interface IPriorizar {
-
-	void priorizar(ArrayList<Tarea> tareas);
-
-}

fc8299b1bd4366b147d6d179f2ca3498efc7accd
vicenterojas05-beep <vicente.rojas05@alumnos.ucn.cl>
2026-09-12 15:42:58 -0300

    Add files via upload

--- Taller_Hospital/main.cpp
+++ Taller_Hospital/main.cpp
@@ -90,6 +90,7 @@ void menu(){
 
 int main(int argc, char** argv) {
 	Queue* cola = new Queue();
+    Stack* historial = new Stack;
     Hospital* hospital = new Hospital();
 	Leerarchivo(cola);
 
@@ -131,10 +132,15 @@ int main(int argc, char** argv) {
             break;
 			}
 
-            case 3:
+            case 3:{
 
-            cout<<"Falta la funcion"<<endl;
+            cout<<"== HISTORIAL DE ÚLTIMAS ATENCIONES DEL HOSPITAL =="<<endl;
+            cout<<endl;
+            historial->mostrar_historial();
+            cout<<endl;
+            
             break;
+            }
 
             case 4:
 
